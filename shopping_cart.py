9edb77c51bbff874cd0fa2d8455009cdd6ef36a8
Bhavin Panchal <bhavinpanchal86@live.com>
2024-03-07 10:49:27 +0000

    added setp 2 simple offers  with unit tests

--- shopping_cart.py
+++ shopping_cart.py
@@ -7,7 +7,14 @@ class ShoppingCart:
         self.items.append(item)
 
     def calculate_total(self):
-        total_cost = 0
-        for item in self.items:
-            total_cost += self.prices.get(item, 0)
+        apple_count = self.items.count('Apple')
+        orange_count = self.items.count('Orange')
+
+        # Buy one, get one free on apples
+        apple_cost = (apple_count // 2 + apple_count % 2) * self.prices['Apple']
+
+        # 3 for the price of 2 on oranges
+        orange_cost = (orange_count // 3 * 2 + orange_count % 3) * self.prices['Orange']
+
+        total_cost = apple_cost + orange_cost
         return round(total_cost, 2)
--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -20,5 +20,30 @@ class TestShoppingCart(unittest.TestCase):
         self.assertEqual(self.cart.calculate_total(), 0)
 
 
+class TestShoppingCartWithOffers(unittest.TestCase):
+    def setUp(self):
+        self.cart = ShoppingCart()
+
+    def test_buy_one_get_one_free_on_apples(self):
+        self.cart.scan('Apple')
+        self.cart.scan('Apple')
+        self.assertEqual(self.cart.calculate_total(), 0.60)
+
+    def test_3_for_price_of_2_on_oranges(self):
+        self.cart.scan('Orange')
+        self.cart.scan('Orange')
+        self.cart.scan('Orange')
+        self.assertEqual(self.cart.calculate_total(), 0.50)
+
+    def test_combined_offers(self):
+        self.cart.scan('Apple')
+        self.cart.scan('Apple')
+        self.cart.scan('Orange')
+        self.cart.scan('Apple')
+        self.cart.scan('Orange')
+        self.cart.scan('Orange')
+        # Allow a small tolerance for rounding differences
+        self.assertAlmostEqual(self.cart.calculate_total(), 1.7)
+
 if __name__ == "__main__":
     unittest.main()
